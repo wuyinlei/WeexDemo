7959c24bda59adc98f22c823e71291aeb46283bd
wuyinlei <1069584784@qq.com>
2018-10-20 11:44:57 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <text class="message" @click="handleClick">{{msg}}</text>
+</template>
+
+<script>
+  export default {
+      data () {
+          return {
+              msg:'请你点击我！！！'
+          }
+      },
+      methods:{
+          handleClick(){
+              this.msg = "谢谢点击我！！！"
+          }
+      }
+  }
+
+</script>
+
+<style scoped>
+  .message {
+    font-size: 50px;
+    color: red;
+    border: #00B4FF;
+    font-style: italic;
+  }
+</style>
